408dcc484c3d4e060829eaa4e2645d2ed2c7389a
thibault froissant <thibault.froissant@gmail.com>
2020-11-28 20:43:24 +0100

    First commit4

---
+++ jaja.js
@@ -0,0 +1,31 @@
+const el = document.querySelector(".test");
+
+el.addEventListener('mousedown', mousedown);
+
+function mousedown(e) {
+
+  window.addEventListener('mousemove', mousemove);
+  window.addEventListener('mouseup', mouseup);
+
+  let prevX = e.clientX;
+  let prevY = e.clientY;
+
+
+  function mousemove(e) {
+    let newX = prevX - e.clientX;
+    let newY = prevY - e.clientY;
+
+    const rect = el.getBoundingClientRect();
+
+    el.style.left = rect.left - newX + "px";
+    el.style.top = rect.top - newY + "px";
+
+    prevX = e.clientX;
+    prevY = e.clientY;
+
+  }
+
+  function mouseup() {
+
+
+}
